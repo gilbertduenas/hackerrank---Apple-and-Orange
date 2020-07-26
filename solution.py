c732861f7201d4ffd060dcc47f2feac759d5c89b
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-26 09:41:09 +0900

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,46 @@
+# s: integer, starting point of Sam's house location.
+# t: integer, ending location of Sam's house location.
+# a: integer, location of the Apple tree.
+# b: integer, location of the Orange tree.
+# apples: integer array, distances at which each apple falls from the tree.
+# oranges: integer array, distances at which each orange falls from the tree.
+
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_count = 0
+    orange_count = 0
+
+    for i in apples:
+        pos_a = a+i
+        if pos_a >= s and pos_a <= t:
+            apple_count += 1
+    
+    for j in oranges:
+        pos_o = b+j
+        if pos_o >= s and pos_o <= t:
+            orange_count += 1
+
+    print(apple_count)
+    print(orange_count)
+
+if __name__ == '__main__':
+    st = input().split()
+    s = int(st[0])
+    t = int(st[1])
+    ab = input().split()
+    a = int(ab[0])
+    b = int(ab[1])
+    mn = input().split()
+    m = int(mn[0])
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+    oranges = list(map(int, input().rstrip().split()))
+    countApplesAndOranges(s, t, a, b, apples, oranges)
